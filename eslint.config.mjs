3b3d870b41e700a4e412cc47e8fa681e989d07b8
Satish Ghode <satishghode9075@gmail.com>
2025-05-24 18:59:29 +0530

    feat: set-up and configure prettierr with eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,30 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default [
-  {
-    files: ['**/*.ts'],
-    ignores: ['**/build/**', '**/dist/**','**/*.config.js', '**/*.config.mjs', '**/*.cjs'],
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-        allowDefaultProject: true,
-      },
+    {
+        files: ['**/*.ts'],
+        ignores: ['**/build/**', '**/dist/**', '**/*.config.js', '**/*.config.mjs', '**/*.cjs'],
+        languageOptions: {
+            parser: tseslint.parser,
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+                allowDefaultProject: true
+            }
+        },
+        plugins: {
+            '@typescript-eslint': tseslint.plugin,
+            prettier: prettierPlugin
+        },
+        rules: {
+            ...eslint.configs.recommended.rules,
+            ...tseslint.configs.recommendedTypeChecked[0].rules,
+            'no-console': 'error',
+            quotes: ['error', 'single', { allowTemplateLiterals: true }]
+        }
     },
-    plugins: {
-      '@typescript-eslint': tseslint.plugin,
-    },
-    rules: {
-      ...eslint.configs.recommended.rules,
-      ...tseslint.configs.recommendedTypeChecked[0].rules,
-      'no-console': 'error',
-      'quotes': ['error', 'single', { allowTemplateLiterals: true }],
-    },
-  },
+    prettierConfig
 ];
