bf699ff223bc280f58d234b8a57d5682ef458af6
Satish Ghode <satishghode9075@gmail.com>
2025-05-24 21:58:30 +0530

    feat: set-up  the env files

---
+++ src/config/config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+import dotenvFlow from 'dotenv-flow';
+dotenvFlow.config();
+
+
+export default {
+    ENV: process.env.ENV || 'development',
+    PORT: process.env.PORT || 3000,
+    DATABASE_URL: process.env.DATABASE_URL || ''
+};
