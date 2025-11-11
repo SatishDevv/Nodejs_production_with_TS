eceace25249fa24f99680b48468a083f910a6d5f
Satish <satishghode9075@gmail.com>
2025-11-11 10:56:50 +0530

    fix: update the eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,16 @@
 // export default [
 //     {
 //         files: ['**/*.ts'],
-//         ignores: ['**/build/**', '**/dist/**', '**/*.config.js', '**/*.config.mjs', '**/*.cjs'],
+//         ignores: [
+//             '**/build/**',
+//             '**/dist/**',
+//             '**/*.config.js',
+//             '**/*.config.mjs',
+//             '**/*.cjs',
+//             '**/*.yml',
+//             '**/*.json',
+//             '**/*.md'
+//         ],
 //         languageOptions: {
 //             parser: tseslint.parser,
 //             parserOptions: {
@@ -30,17 +39,29 @@
 //     prettierConfig
 // ];
 
-import eslintConfigPrettier from 'eslint-config-prettier';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default [
     {
         files: ['**/*.ts'],
-        ignores: ['**/build/**', '**/dist/**', '**/*.config.js', '**/*.config.mjs', '**/*.cjs', '**/*.yml', '**/*.json', '**/*.md'],
+        ignores: [
+            '**/build/**',
+            '**/dist/**',
+            '**/*.config.js',
+            '**/*.config.mjs',
+            '**/*.cjs',
+            '**/*.yml',
+            '**/*.json',
+            '**/*.md'
+        ],
         languageOptions: {
             parser: tseslint.parser,
             parserOptions: {
                 projectService: true,
-                tsconfigRootDir: import.meta.dirname,
+                tsconfigRootDir: new URL('.', import.meta.url).pathname,
                 allowDefaultProject: true
             }
         },
@@ -52,10 +73,10 @@ export default [
             ...eslint.configs.recommended.rules,
             ...tseslint.configs.recommendedTypeChecked[0].rules,
             'no-console': 'error',
-            'no-useless-catch': 0,
+            'no-useless-catch': 'off',
             quotes: ['error', 'single', { allowTemplateLiterals: true }],
-            'prettier/prettier': ['error']
+            'prettier/prettier': 'error'
         }
     },
-    eslintConfigPrettier
+    prettierConfig
 ];
